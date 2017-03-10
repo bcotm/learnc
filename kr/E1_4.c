67f3cfa4524de2f2a05bc1f2213af94caf9af3aa
bcotm <ybcotm@gmail.com>
2017-03-10 15:47:46 +0800

    another version of this

--- kr/E1_4.c
+++ kr/E1_4.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 
+int get_fahr(float*);
+void print_ce(float);
+
 int main()
 {
-    float fahr;
-
-    while(printf("Please input the fahr:") && scanf("%f", &fahr))
-    {
-        printf("The celsius is %6.2f\n", (5.0/9.0)*(fahr-32.0));
+    float vfahr;
+    float *fahr=&vfahr;
+    while(get_fahr(fahr)){
+	print_ce(vfahr);
     }
+//    while(printf("Please input the fahr:") && scanf("%f", &fahr))
+//    {
+//        printf("The celsius is %6.2f\n", (5.0/9.0)*(fahr-32.0));
+//    }
     return 0;
+}
+
+int get_fahr(float *fahr){
+    printf("Another version ,fahr:");
+    return scanf("%f", fahr);
+}
+
+void print_ce(float fahr){
+    printf("The celsius is %6.2f\n", (5.0/9.0)*(fahr-32.0));
 }
